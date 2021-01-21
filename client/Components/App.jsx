740c104e2b738360b4f177998066457c72524a11
jaimebaik <jaimebaik5@gmail.com>
2021-01-21 10:35:19 -0800

    [add]material ui for date picker

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -2,8 +2,22 @@ import React, { useState, useEffect } from 'react';
 import {csv} from 'd3';
 import sampleData from '../assets/sampleData.csv';
 import bb, {bar} from 'billboard.js';
+import TextField from '@material-ui/core/TextField';
+import {
+  DatePicker,
+  DateRangePicker,
+  DateRangeDelimiter,
+  LocalizationProvider,
+  MuiPickersUtilsProvider
+} from "@material-ui/pickers";
+// import DateFnsUtils from "@material-ui/pickers/adapter/date-fns";
+import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
+  const [date, setDate] = useState(['x', '1/11/2021', '1/12/2021', '1/13/2021', '1/14/2021', '1/15/2021', '1/16/2021']);
+  const [current, setCurrent] = useState(['current', 30, 200, 100, 170, 150, 250]);
+  const [compare, setCompare] = useState(['compare', 130, 100, 140, 35, 110, 50]);
+
   useEffect(()=>{
     csv(sampleData)
     .then(data => {
@@ -14,7 +28,7 @@ function App() {
     )
   },[])
   let chart = bb.generate({
-    bindto: "#chart",
+    bindto: '#chart',
     interaction: {
       enabled: false
     },
@@ -22,23 +36,39 @@ function App() {
        x: 'x',
         type: bar(),
         columns: [
-            ['x', '1/11/2021', '1/12/2021', '1/13/2021', '1/14/2021', '1/15/2021', '1/16/2021'],
-            ["current", 30, 200, 100, 170, 150, 250],
-            ["compare", 130, 100, 140, 35, 110, 50]
+            date,
+            current,
+            compare
         ],
-        xFormat: "%m/%d/%Y"
+        xFormat: '%m/%d/%Y'
     },
-    "axis": {
-			"x": {
-				"type": "timeseries",
+    'axis': {
+			'x': {
+				'type': 'timeseries',
 			}
 		}
   })
 
+
+
   return (
     <>
       <div>bye</div>
-      <div id="chart"></div>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <div className='DatePicker'>
+          <div className='currentDatePicker'>
+            <DatePicker />
+            <div>to</div>
+            <DatePicker />
+          </div>
+          <div className='compareDateTransition'>
+            <DatePicker />
+            <div>to</div>
+            <DatePicker />
+          </div>
+        </div>
+      </MuiPickersUtilsProvider>
+      <div id='chart'></div>
     </>
   )
 }
