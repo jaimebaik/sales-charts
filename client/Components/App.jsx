1d2a343b813a489bf25e9c6833e23a626db1dead
jaimebaik <jaimebaik5@gmail.com>
2021-01-22 16:42:17 -0800

    get the correct bar chart with user input

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -2,15 +2,10 @@ import React, { useState, useEffect } from 'react';
 import {csv} from 'd3';
 import sampleData from '../assets/sampleData.csv';
 import bb, {bar} from 'billboard.js';
-// import TextField from '@material-ui/core/TextField';
 import {
   DatePicker,
-  // DateRangePicker,
-  // DateRangeDelimiter,
-  // LocalizationProvider,
   MuiPickersUtilsProvider
 } from "@material-ui/pickers";
-// import DateFnsUtils from "@material-ui/pickers/adapter/date-fns";
 import DateFnsUtils from '@date-io/date-fns';
 import '../styles/styles.css';
 
@@ -23,36 +18,66 @@ function App() {
   const [currEndDate, setCurrEndDate] = useState(null);
   const [compStartDate, setCompStartDate] = useState(null);
   const [compEndDate, setCompEndDate] = useState(null);
-  const [currData, setCurrData] = useState([]);
-  const [compData, setCompData] = useState([]);
-  const [date, setDate] = useState(['x', '1/11/2021 ', '1/12/2021 1/12/2021', '1/13/2021 1/13/2021', '1/14/2021 1/14/2021', '1/15/2021 1/15/2021', '1/16/2021 1/16/2021']);
-  const [current, setCurrent] = useState(['current', 30, 200, 100, 170, 150, 250]);
-  const [compare, setCompare] = useState(['compare', 130, 100, 140, 35, 110, 50]);
+  const [currRange, setCurrRange] = useState([]);
+  const [compRange, setCompRange] = useState([]);
+  const [date, setDate] = useState(['x']);
+  const [current, setCurrent] = useState(['current']);
+  const [compare, setCompare] = useState(['compare']);
+
+  //restriction for comparison date
+  let yesterday = new Date(currStartDate);
+
+  //helper function to get range between dates, inclusive
+  const getDatesBetweenDates = (startDate, endDate) => {
+    let dates = [];
+    //to avoid modifying the original date
+    const theDate = new Date(startDate);
+    const lastDate = new Date(endDate);
+    lastDate.setDate(lastDate.getDate() + 1)
+    while (theDate < lastDate) {
+      const newDate = new Date(theDate);
+      const dateStr = ((newDate.getMonth() > 8) ? (newDate.getMonth() + 1) : ((newDate.getMonth() + 1))) + '/' + ((newDate.getDate() > 9) ? newDate.getDate() : (newDate.getDate())) + '/' + newDate.getFullYear().toString();
+      dates = [...dates, dateStr]
+      theDate.setDate(theDate.getDate() + 1)
+    }
+    return dates;
+  }
 
   //when the component mounts, parse through csv file to save the data in the state
   useEffect(() => {
     csv(sampleData)
     .then(data => {
-      // console.log(data);
-      setRevenues(() => {
-        return [data]
-      })
+      setRevenues(() => data);
     })
     .catch(error => 
       console.log(error)
     )
   },[])
 
+  //get the range of dates when the user changes the inputs
   useEffect(()=> {
-    console.log('Current Start', currStartDate);
-    console.log('Comparison Start', compStartDate);
-    console.log('Current End', currEndDate);
-    console.log('Comparison End', compEndDate);
+    let currNewRange;
+    let compNewRange;
+    if(currStartDate && currEndDate){
+      currNewRange = getDatesBetweenDates(currStartDate, currEndDate);
+      setCurrRange(currNewRange);
+    }
+    if(compStartDate && compEndDate){
+      compNewRange = getDatesBetweenDates(compStartDate, compEndDate);
+      setCompRange(compNewRange);
+    }
+    let dateRange = ['x'];
+    if(currNewRange){
+      for(let i = 0; i < currNewRange.length; i++){
+        dateRange.push(currNewRange[i]);
+        if(compNewRange) compNewRange[i] ? dateRange[i+1] += '\n'.concat(compNewRange[i]) : dateRange[i+1] += ''; 
+      }
+      setDate(dateRange);
+    }
   }, [currStartDate, compStartDate, currEndDate, compEndDate])
   
   //everytime date/currData/compData state change, regenerate the chart with new data
   useEffect(()=>{
-    console.log(date);
     let chart = bb.generate({
       bindto: '#chart',
       interaction: {
@@ -82,7 +107,6 @@ function App() {
           'type': 'category',
           'tick': {
             rotate: -45,
-            // height: 100
           },
         },
         'y': {
@@ -93,8 +117,29 @@ function App() {
         }
       }
     })
-  }, [date, currData, compData])
+  }, [date, current, compare])
 
+  //use the ranges to grab the correct data to be displayed
+  useEffect(() => {
+    let i = 1;
+    let j = 0;
+    let k = 0;
+    let newCurr = ['current'];
+    let newComp = ['compare'];
+    while(i < revenues.length && j < currRange.length){
+      if(revenues[i]['Date'] === currRange[j]) {
+        newCurr.push(parseFloat(revenues[i]['Revenue Booked'].slice(1).replace(/\,/g, '')));
+        j++;
+      }
+      if(revenues[i]['Date'] === compRange[k]){
+        newComp.push(parseFloat(revenues[i]['Revenue Booked'].slice(1).replace(/\,/g, '')));
+        k++;
+      }
+      i++;
+    }
+    setCurrent(newCurr);
+    setCompare(newComp);
+  }, [currRange, compRange])
   return (
     <>
       <div className='bye'>bye</div>
@@ -108,26 +153,16 @@ function App() {
               maxDate={new Date(2021, 2, 31)}
               value={currStartDate} 
               onChange={(newDate)=> {
-                let startDate = ((newDate.getMonth() > 8) ? (newDate.getMonth() + 1) : ((newDate.getMonth() + 1))) + '/' + ((newDate.getDate() > 9) ? newDate.getDate() : (newDate.getDate())) + '/' + newDate.getFullYear();
-                setCurrStartDate(startDate);
+                setCurrStartDate(newDate);
               }}
             />
             <div className='transition'>to</div>
             <DatePicker 
               className='curr' 
-              minDate={new Date(2021, 0, 1)}
+              minDate={currStartDate}
               maxDate={new Date(2021, 2, 31)}
               value={currEndDate} 
-              onChange={(newDate)=> {
-                let endDate = ((newDate.getMonth() > 8) ? (newDate.getMonth() + 1) : ((newDate.getMonth() + 1))) + '/' + ((newDate.getDate() > 9) ? newDate.getDate() : (newDate.getDate())) + '/' + newDate.getFullYear();
-                setCurrEndDate(endDate);
-                let newDateArr = ['x'];
-                currStartDate
-                currEndDate
-                if(currStartDate.getMonth() === currEndDate.getMonth){
-
-                }
-              }}
+              onChange={newDate=> setCurrEndDate(newDate)}
             />
           </div>
           {(currStartDate&&currEndDate) ? <></> : <div className='red'>required to select start and end dates for current data</div>}
@@ -135,15 +170,17 @@ function App() {
           <label>compare: </label>
             <DatePicker 
               minDate={new Date(2021, 0, 1)}
-              maxDate={currStartDate}
+              maxDate={currStartDate ? yesterday.setDate(currStartDate.getDate() - 1) : currStartDate}
               value={compStartDate} 
-              onChange={(newDate) => {
-                let startDate = ((newDate.getMonth() > 8) ? (newDate.getMonth() + 1) : ((newDate.getMonth() + 1))) + '/' + ((newDate.getDate() > 9) ? newDate.getDate() : (newDate.getDate())) + '/' + newDate.getFullYear();
-                setCompStartDate(startDate);
-              }}
+              onChange={newDate => setCompStartDate(newDate)}
             />
             <div className='transition'>to</div>
-            <DatePicker />
+            <DatePicker 
+              minDate={compStartDate}
+              maxDate={currStartDate ? yesterday.setDate(currStartDate.getDate() - 1) : currStartDate}
+              value={compEndDate} 
+              onChange={newDate => setCompEndDate(newDate)}
+            />
           </div>
         </div>
       </MuiPickersUtilsProvider>
